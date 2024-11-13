8341cca6e3279081795d98776edb8a4b5c98f31b
daichi0918 <kayasugadaichi@icloud.com>
2024-11-13 16:47:41 +0900

    finish:chap24

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,19 @@
+import SideMenu from '@/components/SideMenu/SideMenu';
+import React from 'react'
+
+const MainLayout = (
+  {
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>
+) => {
+  return (
+    <div className='flex h-screen'>
+      <SideMenu />
+      <div className='bg-red-300 flex-1 overflow-auto'>{children}</div>
+    </div>
+  )
+}
+
+export default MainLayout
